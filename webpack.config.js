b90e75a573bb298f8811c3d56e79c8f8607ff1b9
Igor Vasiliev <helmsman@gmail.com>
2017-03-30 17:52:30 +0300

    rebuild on changes

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,13 @@ module.exports = {
     filename: 'build.js',
     //  after build, export of module will be placed into "home" param
     library: "home"
+  },
+  //  webpack will watch for file changes
+  //  if something will be changed, webpack will run rebuild
+  watch: true,
+
+  //  how much time to wait until run rebuild - default 300ms, changed to 250ms
+  watchOptions: {
+    aggregateTimeout: 250
   }
 }
