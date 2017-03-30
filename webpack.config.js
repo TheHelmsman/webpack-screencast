db1f1a47c4372415ac63ac68d080aa2f31503ac1
Igor Vasiliev <helmsman@gmail.com>
2017-03-30 18:09:15 +0300

    source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,11 @@ module.exports = {
     //  after build, export of module will be placed into "home" param
     library: "home"
   },
+
+  //  ++++++++++++++++++++++++++++++++++
+  //  live rebuild
+  //  ++++++++++++++++++++++++++++++++++
+
   //  webpack will watch for file changes
   //  if something will be changed, webpack will run rebuild
   watch: true,
@@ -15,5 +20,22 @@ module.exports = {
   //  how much time to wait until run rebuild - default 300ms, changed to 250ms
   watchOptions: {
     aggregateTimeout: 250
-  }
+  },
+
+  //  ++++++++++++++++++++++++++++++++++
+  //  sourcemap for production (source-map or cheap-source-map)
+  //  sourcemap for development (eval or cheap-inline-module-source-map)
+  //  ++++++++++++++++++++++++++++++++++
+
+  //  sourcemap will be placed in external build.js.map file
+  //  devtool: "source-map"
+  
+  //  sourcemap will be written into the bottom of build.js file
+  //devtool: "inline-source-map"
+
+  //  sourcemap will be written into the bottom of the build.js file in shorter form
+  // devtool: "cheap-inline-module-source-map"
+
+  // each module will contains eval with param sourceUrl (actual sourcemap will not be build)
+  devtool: "eval"
 }
