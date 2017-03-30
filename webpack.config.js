09f772935b888366a25e76ca329b8193fa33e641
Igor Vasiliev <helmsman@gmail.com>
2017-03-30 17:47:06 +0300

    external access to modules

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@
 module.exports = {
   entry: './home',
   output: {
+    //  filename for builded js file
     filename: 'build.js',
+    //  after build, export of module will be placed into "home" param
+    library: "home"
   }
 }
